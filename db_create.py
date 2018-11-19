45d37fc9815225fe0d968d6d4e0754a1ffa83743
Lans Christopher <lanschristopher@Lanss-MacBook-Pro.local>
2018-11-19 17:07:19 +0530

    new imporrsts

--- db_create.py
+++ db_create.py
@@ -1,8 +1,9 @@
 from app import db
 from app import User
+from flask_sqlalchemy import SQLAlchemy 
 
-#db.create_all()
+db.create_all()
 #db.session.add(User("ABSDCAAAS11","60","ABHYUDAYA NAGAR","ABHYUDAYA EDUCATION SOCIETY, OPP. BLDG. NO. 18, ABHYUDAYA NAGAR, KALACHOWKY, MUMBAI - 400033","MUMBAI","GREATER MUMBAI","MAHARASHTRA","ABHYUDAYA COOPERATIVE BANK LIMITED"))
 #db.session.commit()
 
-print "hello"
+#print "hello"
