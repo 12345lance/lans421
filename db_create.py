a464e585ba5385e00c6f69c25e3d3e76d9003fb0
Lans Christopher <lanschristopher@Lanss-MacBook-Pro.local>
2018-11-19 17:09:53 +0530

    new imporrstsasd

--- db_create.py
+++ db_create.py
@@ -1,7 +1,8 @@
 from app import db
 from app import User
-from flask_sqlalchemy import SQLAlchemy 
 
+
+db = SQLAlchemy(app)
 db.create_all()
 #db.session.add(User("ABSDCAAAS11","60","ABHYUDAYA NAGAR","ABHYUDAYA EDUCATION SOCIETY, OPP. BLDG. NO. 18, ABHYUDAYA NAGAR, KALACHOWKY, MUMBAI - 400033","MUMBAI","GREATER MUMBAI","MAHARASHTRA","ABHYUDAYA COOPERATIVE BANK LIMITED"))
 #db.session.commit()
